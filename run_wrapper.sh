406254c3c00328b682bbb29240fca2544373d497
ir-paras-oli <paras.oli@invisirisk.com>
2025-05-02 11:58:32 +0545

    change

--- run_wrapper.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Starting leak_token.sh execution..."
-bash ./secret_leak.sh  # Call the actual script
-bash ./statuscode.sh # Call the actual script
-bash ./push.sh  # Call the actual script
-bash ./mimetype.sh  # Call the actual script
-echo "Execution completed."
